0c8c37ce16474187d7b07f0f64f718b0ba9c3b52
Vitalii Nikolaienko <vitalii.nikolaienko@student.tuke.sk>
2022-11-02 22:06:47 +0100

    Added default

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const main = document.getElementById("main");
+const text = document.getElementById("text");
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
-console.log(params);
-console.log(params.color);
-main.style.backgroundColor = params.color;
+if (params.color != null) {
+    main.style.backgroundColor = params.color;
+    text.style.visibility = false;
+}
+else if (params.text != null) {
+    main.style.backgroundColor = "white";
+    text.style.visibility = true;
+    text.innerText = params.text;
+}
